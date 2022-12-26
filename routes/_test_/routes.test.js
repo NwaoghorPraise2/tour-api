dd3e53bd6a245e7f38540473d59b78b5a00974b2
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-12-26 21:11:26 +0100

    test

--- routes/_test_/routes.test.js
+++ routes/_test_/routes.test.js
@@ -1,4 +1,13 @@
+const request = require('supertest');
+const app = require('../../app');
+
+jest.setTimeout(5000000);
+
 // eslint-disable-next-line no-undef
-describe('testing for end points', () => {
-  //   expect('Status code 200');
+describe('testing for tours get end point', () => {
+  test('testing response from /get all tours', async () => {
+    const res = await request(app).get('/api/v1/tours/top-5-cheap');
+    console.log(res);
+    expect(res.statusCode).toBe(200);
+  });
 });
