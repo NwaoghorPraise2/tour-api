10d629ced0151cba9e230d1be42d9b69a6980bf6
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-12-27 19:01:26 +0100

    Mongoose Day

--- models/toursModel.js
+++ models/toursModel.js
@@ -101,6 +101,15 @@ tourSchema.post(/^find/, function (docc, next) {
   next();
 });
 
+//Aggregaite Middleware
+//Pre
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } }); 
+  next();
+});
+
+
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
