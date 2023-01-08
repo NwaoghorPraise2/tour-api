b3062c5b9037d6e4d0262fa588357c8c83637c3b
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2023-01-08 18:53:59 +0100

    Implemrnt Auth

--- controller/authController.js
+++ controller/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/usersModel');
 const ansycHandler = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/sendEmail');
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
@@ -100,6 +101,8 @@ const forgotPassword = ansycHandler( async (req, res, next) => {
 
     const resetToken = user.createPasswordResetToken();
     await user.save({ validateBeforeSave: false});
+
+    
 
     
 }); 
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,28 @@
 const nodeMailer = require('nodemailer');
 
-const sendEmail = options => {
-    //1) Transporter
-    
+const sendEmail = async options => {
+    // 1) Transporter
+    const transporter = nodeMailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    // 2) Define Email Options
+    const mailOptions = {
+        from: 'Nwaoghor Praise <hello@nwaoghorpraise.io>',
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        // html:// 2) Define Email Options
+    }
+
+    // 3) Actually Send Mail
+    await transporter.sendMail(mailOptions);
 }
+
+module.exports = sendEmail;
