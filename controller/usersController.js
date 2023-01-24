8b6665b184564ee5abedc9d03535033378156418
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2023-01-24 14:29:19 +0100

    Commit

--- controller/usersController.js
+++ controller/usersController.js
@@ -2,6 +2,15 @@ const User =  require('../models/usersModel');
 const AppError = require('../utils/appError');
 const ansycHandler = require('../utils/catchAsync');
 
+const filterInput = (obj, ...allowedFields) => {
+  const filteredObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) filteredObj[el] = obj[el];
+  })
+
+  return filteredObj;
+};
+
 const getAllUsers = ansycHandler( async (req, res, next) => {
 
   const users = await User.find();
@@ -20,9 +29,15 @@ const updateMe = ansycHandler(async (req, res, next)=> {
     return next( new AppError('You cannot update password from this route', 400));
   }
 
+  const filtedReq = filterInput(req.body,'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filtedReq, { new: true, runValidators: true});
+
   res.status(200).json({
     status: 'success',
-    message: 'User Profile Updated'
+    message: 'User Profile Updated',
+    data: {
+      user: updatedUser
+    }
   });
 });
 
